9244f7c880391c9d28a72352da084eff1e28cbdd
wiryajaya <wir_fam17@hotmail.com>
2020-12-17 05:01:01 +0800

    create home page

--- einvitation/urls.py
+++ einvitation/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+import weddingpage.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('weddingpage.urls')),
+#    path('',include('weddingpage.urls')),
+    path('', weddingpage.views.home, name='home'),
+    path('blissful/',include('weddingpage.urls')),
 ]
--- weddingpage/views.py
+++ weddingpage/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def blissful(request):
+    return render(request, 'blissful.html', {})
