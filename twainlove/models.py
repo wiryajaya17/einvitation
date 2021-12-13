b94019f862df6fb0d8089cd24658a541cf5f5e12
wiryajaya <wir_fam17@hotmail.com>
2021-12-13 13:38:20 +0800

    tes fitur testimoni

--- twainlove/models.py
+++ twainlove/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Rsvp(models.Model):
+    name = models.CharField(max_length=60)
+    email = models.CharField(max_length=60)
+    num_of_rsvp = models.IntegerField()
+    flag_attending = models.CharField(max_length=1)
+    message_body = models.CharField(max_length=200)
+    post_date = models.DateTimeField(default=timezone.now)
--- twainlove/views.py
+++ twainlove/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from .models import Rsvp
 
 def twainlove(request):
-    return render(request, 'twainlove.html', {})
+    rsvps = Rsvp.objects
+    return render(request, 'twainlove.html', {'rsvps' : rsvps})
+
+def create(request):
+    if request.method == 'POST':
+        rsvp = Rsvp()
+        rsvp.name = request.POST['name']
+        rsvp.email = request.POST['email']
+        rsvp.num_of_rsvp = request.POST['num_of_rsvp']
+        rsvp.flag_attending = request.POST['flag_attending']
+        rsvp.message_body = request.POST['message_body']
+        rsvp.save()
+        return render(request,'create.html', {})
+    else:
+        return render(request, 'twainlove.html', {})
+    # return render(request,'create.html', {})
